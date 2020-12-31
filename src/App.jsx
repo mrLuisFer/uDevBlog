e528b560dbe36d2cd6f50d1371e63c5f527b61d9
mrLuisFer <lolesuncrak@gmail.com>
2020-12-30 21:43:13 -0600

    Aniadi React Router para poder navegar entre las paginas de manera sencilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// Views
+import WelcomeView from "./views/Welcome"
+import HomePage from "./views/HomePage"
+
 function App() {
   return (
-    <div className="App">
-      <p>Here start the project</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <WelcomeView />
+          </Route>
+          {/* La view de Welcome redirecciona hacia la HomePage */}
+          <Route path="/home">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
