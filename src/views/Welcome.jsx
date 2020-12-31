a3139e2efd4014854597ca530a374e8a54605918
mrLuisFer <lolesuncrak@gmail.com>
2020-12-31 09:33:24 -0600

    [ADD] una estructura a welcomeView antes de poner los datos y estilos

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,9 +1,29 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
 
 export default function WelcomeView() {
+  let history = useHistory()
+
+  const pushToHome = () => {
+    history.push("/home")
+  }
+
   return (
     <div className="WelcomeView">
-      <p>Welcome view</p>
+      {/* Some hero img */}
+      <h1>Bienvenido a uDevBlog</h1>
+      {/* Line */}
+      <p>
+        En este blog estaran las tecnologias, herramientas de la programacion,
+        asi como comunidades en español
+      </p>
+      {/* Some img */}
+      <div>{/* Some Social media or information */}</div>
+      <div>
+        <button onClick={pushToHome}>
+          Vamos a conocer a mas comunidades🌟
+        </button>
+      </div>
     </div>
   )
 }
