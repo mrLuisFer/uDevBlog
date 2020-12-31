9a1200bec9e70ad67aaf7b5f911ded4da12d1ca4
mrLuisFer <lolesuncrak@gmail.com>
2020-12-30 19:53:18 -0600

    Adding node-sass for the styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from "./logo.svg"
-import "./App.css"
+import "./App.scss"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Here start the project</p>
     </div>
   )
 }
