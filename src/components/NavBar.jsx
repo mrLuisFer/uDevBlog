6611caafdcd1697b5d130aaf23d91dddfa4161d9
mrLuisFer <lolesuncrak@gmail.com>
2021-01-03 00:34:45 -0600

    font awesome CDN agregado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,10 @@
-import React from "react"
+import React from 'react';
 
 export default function NavBar() {
   return (
-    <div className="NavBar">
+    <div className='NavBar'>
       <span>logo</span>
-      {/* Posiblemente una barra de busqueda */}
-      <div>
-        <span>Links</span>
-      </div>
+      <i className='fas fa-hamburger'></i>
     </div>
-  )
+  );
 }
