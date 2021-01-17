748c84eb24a6c36889b4b52604f500ff417c326c
mrLuisFer <lolesuncrak@gmail.com>
2021-01-17 01:46:10 -0600

    Añdi una ilustracion para el noPostFounded

--- src/components/NoPostsFounded/NoPostsFounded.jsx
+++ src/components/NoPostsFounded/NoPostsFounded.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+// Assets
+import noPostsIllustration from '../../assets/no-posts/no-posts-ill.svg';
 
 export default function NoPostsFounded() {
   return (
     <div className='NoPostsFounded'>
       <p>No posts founded</p>
+      <img src={noPostsIllustration} alt='no-posts-founded' />
     </div>
   );
 }
