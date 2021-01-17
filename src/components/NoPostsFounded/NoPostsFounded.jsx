523dfce2ea163e6893e6020a9328dbb31091d0b5
mrLuisFer <lolesuncrak@gmail.com>
2021-01-17 16:01:37 -0600

    añadi contenido y estilos para cuando no haya posts encontrados

--- src/components/NoPostsFounded/NoPostsFounded.jsx
+++ src/components/NoPostsFounded/NoPostsFounded.jsx
@@ -5,8 +5,24 @@ import noPostsIllustration from '../../assets/no-posts/no-posts-ill.svg';
 export default function NoPostsFounded() {
   return (
     <div className='NoPostsFounded'>
-      <p>No posts founded</p>
-      <img src={noPostsIllustration} alt='no-posts-founded' />
+      <div>
+        <h2 className='NoPostsFounded__title'>Oops!</h2>
+        <p className='NoPostsFounded__text'>
+          No hay resultados.. {':('} <br />
+          <span className='NoPostsFounded__text-span'>
+            Pero puedes buscar en estos lugares {':D'}
+          </span>
+        </p>
+        <div className='NoPostsFounded__container'>
+          <button>Comunidades</button>
+          <button>Descubrir</button>
+        </div>
+      </div>
+      <img
+        className='NoPostsFounded__img'
+        src={noPostsIllustration}
+        alt='no-posts-founded'
+      />
     </div>
   );
 }
