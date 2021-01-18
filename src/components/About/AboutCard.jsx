b8ba50740cfbc2ca1df2fc5f4925b159cf2813ed
LCV13 <lourdesvillarreal109@gmail.com>
2021-01-18 19:47:58 -0300

    Importé AboutCard en AboutText y los estilos en _index.scss

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+// Utils
+import {fadeIn} from '../../utils/animateCss/index';
+
     export default function AboutCard(){
     return (
-        <div>
-            
+        <div className={fadeIn + 'AboutCard'}>
+            <div className="AboutCard__container">
+                <div className="AboutCard__image">
+                <img src='https://picsum.photos/100' className="AboutCard__image-img"></img>
+                </div>
+                <div className="AboutCard__text">
+                    <h3 className="AboutCard__text-title">Nombre</h3>
+                    <p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.</p>
+                </div>
+                <div className="redes-sociales">
+                <a href="#">IG</a>
+                <a href="#">IG</a>
+                <a href="#">IG</a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/About/AboutText.jsx
+++ src/components/About/AboutText.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 // Utils
 import {fadeIn} from '../../utils/animateCss/index';
 
+//Components
+import AboutCard from './AboutCard'
+
 export default function AboutText() {
     return (
         <>
             <div className={fadeIn + 'AboutText'}>
                 <h1>About</h1>
                 <p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo Contenido aquí, contenido aquí. Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de Lorem Ipsum va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito por ejemplo insertándole humor y cosas por el estilo.</p>
+                <AboutCard/>
             </div>
         </>
     )
