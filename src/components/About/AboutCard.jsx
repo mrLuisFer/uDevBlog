693e407aaca2d75f45a1e0cd0b2c9c52b6ae441f
LCV13 <lourdesvillarreal109@gmail.com>
2021-01-18 21:34:49 -0300

    Creé las card

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // Utils
 import {fadeIn} from '../../utils/animateCss/index';
 
-    export default function AboutCard(){
+    export default function AboutCard(props){
     return (
         <div className={fadeIn + 'AboutCard'}>
             <div className="AboutCard__container">
@@ -11,13 +11,13 @@ import {fadeIn} from '../../utils/animateCss/index';
                 <img src='https://picsum.photos/100' className="AboutCard__image-img"></img>
                 </div>
                 <div className="AboutCard__text">
-                    <h3 className="AboutCard__text-title">Nombre</h3>
+                    <h2 className="AboutCard__text-title">{props.name}</h2>
                     <p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.</p>
                 </div>
-                <div className="redes-sociales">
-                <a href="#">IG</a>
-                <a href="#">IG</a>
-                <a href="#">IG</a>
+                <div>
+                <a href={props.linkedin} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
+                <a href={props.instagram} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
+                <a href="#" className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </div>
--- src/components/About/AboutText.jsx
+++ src/components/About/AboutText.jsx
@@ -12,7 +12,9 @@ export default function AboutText() {
             <div className={fadeIn + 'AboutText'}>
                 <h1>About</h1>
                 <p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo Contenido aquí, contenido aquí. Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de Lorem Ipsum va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito por ejemplo insertándole humor y cosas por el estilo.</p>
-                <AboutCard/>
+                <AboutCard name="Luis Alvarez" linkedin="https://www.linkedin.com/in/mrLuisFer" github="https://github.com/mrLuisFer" twitter="https://twitter.com/lolesuncrak"/>
+                <AboutCard name="Catalina Villarreal" linkedin="https://www.linkedin.com/in/vcatalina/" github=""/>
+                <AboutCard name="Michael Hernández" instagram="https://www.instagram.com/michaeldelmedio/" linkedin="www.linkedin.com/in/michael-hernandez-sanchez-6ba0b0162" github="https://github.com/elmichaelv"/>
             </div>
         </>
     )
