89a7c633c60a64e8be3ee5d02589749ad4126929
LCV13 <lourdesvillarreal109@gmail.com>
2021-01-19 19:54:00 -0300

    Cambié el manejo de props en AboutCard.jsx

--- src/components/About/AboutCard/AboutCard.jsx
+++ src/components/About/AboutCard/AboutCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // Utils
 import {fadeIn} from '../../../utils/animateCss/index';
 
-    export default function AboutCard(props){
+    export default function AboutCard({name, text, social1, social2, social3}){
     return (
         <div className={fadeIn + 'AboutCard'}>
             <div className="AboutCard__container">
@@ -11,13 +11,13 @@ import {fadeIn} from '../../../utils/animateCss/index';
                 <img src='https://picsum.photos/100' className="AboutCard__image-img"></img>
                 </div>
                 <div className="AboutCard__text">
-                    <h2 className="AboutCard__text-title">{props.name}</h2>
-                    <p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.</p>
+                    <h2 className="AboutCard__text-title">{name}</h2>
+                    <p>{text}</p>
                 </div>
                 <div>
-                <a href={props.linkedin} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
-                <a href={props.instagram} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
-                <a href="#" className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
+                <a href={social1} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
+                <a href={social2} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
+                <a href={social3} className="AboutCard__social"><i className="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </div>
--- src/components/About/AboutText/AboutText.jsx
+++ src/components/About/AboutText/AboutText.jsx
@@ -12,12 +12,22 @@ export default function AboutText() {
             <div className={fadeIn + 'AboutText'}>
                 <h1>About</h1>
                 <p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo Contenido aquí, contenido aquí. Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de Lorem Ipsum va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito por ejemplo insertándole humor y cosas por el estilo.</p>
-                
-                <AboutCard name="Luis Alvarez" social1="https://www.linkedin.com/in/mrLuisFer" social2="https://github.com/mrLuisFer" social3="https://twitter.com/lolesuncrak"/>
 
-                <AboutCard name="Catalina Villarreal" social1="https://www.linkedin.com/in/vcatalina/" social2="https://github.com/LCV13"/>
+                <AboutCard name="Luis Alvarez"
+                    text="texto de Luis"
+                    social1="https://www.linkedin.com/in/mrLuisFer" 
+                    social2="https://github.com/mrLuisFer" 
+                    social3="https://twitter.com/lolesuncrak"/>
 
-                <AboutCard name="Michael Hernández" social1="www.linkedin.com/in/michael-hernandez-sanchez-6ba0b0162" social2="https://github.com/elmichaelv" social3="https://www.instagram.com/michaeldelmedio/"  />
+                <AboutCard name="Catalina Villarreal" 
+                    text="texto de catalina"
+                    social1="https://www.linkedin.com/in/vcatalina/" 
+                    social2="https://github.com/LCV13"/>
+
+                <AboutCard name="Michael Hernández" 
+                    text="texto de Michael"
+                    social1="www.linkedin.com/in/michael-hernandez-sanchez-6ba0b0162" social2="https://github.com/elmichaelv" 
+                    social3="https://www.instagram.com/michaeldelmedio/"/>
             </div>
         </>
     )
